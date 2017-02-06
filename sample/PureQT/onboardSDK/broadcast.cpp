39af553512284f45e1cf0e58af41db62e940954f
William.Wu <git@justwillim.com>
2017-02-06 16:12:20 +0800

    UI update function pre-implementation

--- sample/PureQT/onboardSDK/broadcast.cpp
+++ sample/PureQT/onboardSDK/broadcast.cpp
@@ -7,7 +7,28 @@ using namespace DJI::onboardSDK;
 
 Broadcast::Broadcast(QWidget *parent) : QWidget(parent), ui(new Ui::Broadcast) {
   ui->setupUi(this);
-  qDebug() << "UID: " << Data::DataBase[1].uid;
 }
 
 Broadcast::~Broadcast() { delete ui; }
+
+void Broadcast::displayData(CoreAPI *api, Header *header, UserData data) {
+  Broadcast *This  = (Broadcast *)data;
+  uint8_t *  pdata = ((uint8_t *)header) + sizeof(Header);
+  pdata += 2;
+  uint16_t passFlag = *(uint16_t *)pdata;
+  This->displayGlobalStamp(passFlag);
+  This->displaySyncStamp(passFlag);
+  This->displayQuaternion(passFlag);
+  This->displayAcceleration(passFlag);
+  This->displayVelocity(passFlag);
+  This->displayPalstance(passFlag);
+  This->displayGlobalPosition(passFlag);
+  This->displayRelativePosition(passFlag);
+  This->displayGPS(passFlag);
+  This->displayRTK(passFlag);
+  This->displayMag(passFlag);
+  This->displayRC(passFlag);
+  This->displayGimbal(passFlag);
+  This->displayBattery(passFlag);
+  This->displaySDKInfo(passFlag);
+}
--- sample/PureQT/onboardSDK/broadcast.h
+++ sample/PureQT/onboardSDK/broadcast.h
@@ -17,10 +17,34 @@ class Broadcast : public QWidget {
   ~Broadcast();
 
  public:
-  void setAPI(DJI::onboardSDK::CoreAPI *api) { data.setAPI(api); }
+  void setAPI(DJI::onboardSDK::CoreAPI *api) {
+    data.setAPI(api);
+    data.setBroadcastCallback(displayData, this);
+  }
+
+ public:
+  static void displayData(DJI::onboardSDK::CoreAPI *api,
+                          DJI::onboardSDK::Header *header, DJI::UserData data);
 
  private:
-  Ui::Broadcast *ui;
+  void displayGlobalStamp(uint16_t flag) {}
+  void displaySyncStamp(uint16_t flag) {}
+  void displayQuaternion(uint16_t flag) {}
+  void displayAcceleration(uint16_t flag) {}
+  void displayVelocity(uint16_t flag) {}
+  void displayPalstance(uint16_t flag) {}
+  void displayGlobalPosition(uint16_t flag) {}
+  void displayRelativePosition(uint16_t flag) {}
+  void displayGPS(uint16_t flag) {}
+  void displayRTK(uint16_t flag) {}
+  void displayMag(uint16_t flag) {}
+  void displayRC(uint16_t flag) {}
+  void displayGimbal(uint16_t flag) {}
+  void displayBattery(uint16_t flag) {}
+  void displaySDKInfo(uint16_t flag) {}
+
+ private:
+  Ui::Broadcast *                ui;
   DJI::onboardSDK::DataBroadcast data;
 };
 
