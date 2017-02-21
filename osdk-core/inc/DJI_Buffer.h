7aeb27b2fec78a65a8616ab01ea211ceef9b675c
William.Wu <git@justwillim.com>
2017-02-21 11:05:44 +0800

    readd list method template

--- osdk-core/inc/DJI_Buffer.h
+++ osdk-core/inc/DJI_Buffer.h
@@ -5,46 +5,46 @@
 
 namespace DJI {
 
-// template <typename T>
-// class LoopQueue {
-// public:
-//  LoopQueue();
+template <typename T>
+class LoopQueue {
+ public:
+  LoopQueue();
 
-// public:
-//  bool write(T data);
-//  bool read(T &data);
-//  int write(T *data, int len);
-//  int read(T *data, int len);
+ public:
+  bool write(T data);
+  bool read(T &data);
+  int write(T *data, int len);
+  int read(T *data, int len);
 
-// public:
-//  bool isReadable() const;
-//  bool isWriteable() const;
-//  bool isEmpty() const;
-//  bool isFull() const;
-//  int  length() const;
-//  int  size() const;
-//  void flush(int len = 0);
+ public:
+  bool isReadable() const;
+  bool isWriteable() const;
+  bool isEmpty() const;
+  bool isFull() const;
+  int  length() const;
+  int  size() const;
+  void flush(int len = 0);
 
-// private:
-//  bool isOnHold(int pos);
+ private:
+  bool isOnHold(int pos);
 
-// private:
-//  volatile bool readable : 1;
-//  volatile bool writeable : 1;
-//  volatile T *  memory;
-//  int           size;
-//  volatile int  readFrom;
-//  volatile int  writeTo;
-//};
+ private:
+  volatile bool readable : 1;
+  volatile bool writeable : 1;
+  volatile T *  memory;
+  int           alloc;
+  volatile int  readFrom;
+  volatile int  writeTo;
+};
 
-// template <typename T>
-// class StaticLoopQueue : public LoopQueue<T> {
-// public:
-//  StaticLoopQueue();
-//};
+template <typename T>
+class StaticLoopQueue : public LoopQueue<T> {
+ public:
+  StaticLoopQueue();
+};
 
-// typedef LoopQueue<uint8_t>       Buffer;
-// typedef StaticLoopQueue<uint8_t> StaticBuffer;
+typedef LoopQueue<uint8_t>       Buffer;
+typedef StaticLoopQueue<uint8_t> StaticBuffer;
 
 }  // namespace  DJI
 
